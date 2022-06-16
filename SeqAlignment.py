61f122a23c6aff7fb7c26ce253e2bd026557ff8c
Zeal Jinwala <zsj24@drexel.edu>
2022-06-16 17:19:33 -0400

    removing files

--- SeqAlignment.py
+++ SeqAlignment.py
@@ -1,1 +1,14 @@
-# HW8
+# By Zeal Jinwala
+# Date: February 02, 2022
+# Data source: 
+
+# Read Bam file
+# Counting/Printing the Number of Aligned Sequence Reads (5 points)
+# Plot the Length Distribution of  Aligned Sequence Reads (5 points)
+# Plot  the Mapping Quality Score Histogram (5 points)
+# Use BioRead to read SAM file
+# Plot  the Histogram of Median Base Quality Score  and the Boxplot of Average  Base Quality Score from above SAM file (5 points)
+# Coverage and Consensus sequences (if your laptop is not powerful enough, please do the first 200 bases and first 200 reads)
+# Plot the Coverage of the Sequencing Reads (take-home programming 20 points)
+# Generate the Consensus Sequence (take-home programming 20 points).  
+
